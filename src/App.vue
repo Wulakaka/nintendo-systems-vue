cd2758e516a508090e0954eb2791f4e6ad0d29d9
张伟峰 <zhang.weifeng@tslsmart.com>
2023-05-08 10:40:50 +0800

    feat: 增加路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,14 @@
-<script setup lang="ts">
-import TheEditor from '@/components/editor/components/TheEditor.vue'
-import ThePreview from '@/components/preview/ThePreview.vue'
-
-import RippleCanvas2 from '@/components/ripple-canvas/RippleCanvas2.vue'
-import TheLoading from '@/components/loading/TheLoading.vue'
-import { onMounted, ref } from 'vue'
-import LoadingParallel from '@/models/LoadingParallel'
-import chain from '@/utils/chain'
-
-const refLoading = ref()
-
-onMounted(() => {
-  const p = LoadingParallel.getInstance()
-  chain(
-    () => refLoading.value.show(),
-    () => {
-      return p.start(refLoading.value.update, 1e3)
-    }
-  )
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="app">
-    <TheEditor></TheEditor>
-    <ThePreview></ThePreview>
+    <router-view></router-view>
   </div>
-  <TheLoading ref="refLoading"></TheLoading>
-  <!--  <div class="app__ripple">
-    <RippleCanvas2></RippleCanvas2>
-  </div>-->
 </template>
 
 <style scoped lang="scss">
 .app {
   position: relative;
   height: 100vh;
-
-  &__ripple {
-    position: absolute;
-    left: 0;
-    top: 0;
-  }
 }
 </style>
