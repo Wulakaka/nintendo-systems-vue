92adeca16617d9d1666905c285bc4c2134b3a069
张伟峰 <zhang.weifeng@tslsmart.com>
2023-05-12 18:06:53 +0800

    feat(nav): 增加反向跳动

--- src/views/navigation/TheNavigation.vue
+++ src/views/navigation/TheNavigation.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import NavRound from '@/views/navigation/NavRound.vue'
 import NavigationItem from '@/views/navigation/NavigationItem.vue'
+import NavRoundInner from '@/views/navigation/NavRoundInner.vue'
 </script>
 <template>
   <div class="flex">
     <NavRound></NavRound>
+    <NavRoundInner></NavRoundInner>
   </div>
   <div class="flex">
     <NavigationItem></NavigationItem>
