e34d9429f63e0ce8d59a31da1ef3e2a1ce068b1c
张伟峰 <zhang.weifeng@tslsmart.com>
2023-05-16 15:39:02 +0800

    build: 调整打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'docs'
-  },
+  base: '/nintendo-systems-vue/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
